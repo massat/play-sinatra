6290606cf9a5e49be9883c21ed50e575a776dce8
massat <masato.hirai@gmail.com>
2017-02-02 22:33:58 +0900

    パスワードのカラム

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 create table member (
     member_id BINARY(8) NOT NULL,
     user_name VARCHAR(32) NOT NULL,
+    password VARBINARY(156) NOT NULL,
     created DATETIME NOT NULL,
     PRIMARY KEY (`member_id`)
 ) ENGINE=Innodb DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci;
