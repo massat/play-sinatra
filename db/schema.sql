ed3640d3b14ee10f284b20b57fb05e3861d06a5e
massat <masato.hirai@gmail.com>
2017-02-02 16:11:30 +0900

    初期SQL

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+create table member (
+    member_id BINARY(8) NOT NULL,
+    user_name VARCHAR(32) NOT NULL,
+    created DATETIME NOT NULL,
+    PRIMARY KEY (`member_id`)
+) ENGINE=Innodb DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci;
