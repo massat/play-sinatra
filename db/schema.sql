559ad2987680eedbf67fa670897642874bf3e36d
massat <masato.hirai@gmail.com>
2017-02-03 23:31:32 +0900

    Fix columns

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,7 @@ create table member (
     member_id BINARY(8) NOT NULL,
     user_name VARCHAR(32) NOT NULL,
     password VARBINARY(156) NOT NULL,
-    created DATETIME NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
     PRIMARY KEY (`member_id`)
 ) ENGINE=Innodb DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci;
